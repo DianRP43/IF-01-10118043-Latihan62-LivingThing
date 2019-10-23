f9510fa0e72a47c50c9111a37dd98c281f9fd396
Dian Rosa Pratama <55683288+DianRP43@users.noreply.github.com>
2019-10-23 15:53:48 +0700

    Add files via upload

---
+++ IF-01-10118043-Latihan62-LivingThing/src/pkgif/pkg01/pkg10118043/latihan62/livingthing/LivingThing.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118043.latihan62.livingthing;
+
+/**
+ *
+ * @author user
+ */
+public abstract class LivingThing {
+    public abstract void walk(String nama);
+    public void breath(String nama){};
+    public void eat(String nama){};    
+}
---
+++ IF-01-10118043-Latihan62-LivingThing/src/pkgif/pkg01/pkg10118043/latihan62/livingthing/main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118043.latihan62.livingthing;
+
+/**
+ *
+ * @author user
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Human h = new Human();
+        h.walk("Dian");
+        h.eat("Dian");
+        h.breath("Dian");
+    }
+    
+}
